a6a7a937d64f1679a0b0675771b6d245d1740696
Tosin Akinro <tos.akinro@gmail.com>
2024-03-17 09:27:01 -0500

    uploading terraform files and python script for automated time stamped uploads to s3

---
+++ terraform_files/main.tf
@@ -0,0 +1,25 @@
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-yahoo-bucket-name-${random_string.suffix.result}"
+
+}
+
+resource "aws_s3_bucket_versioning" "my_bucket_versioning" {
+  bucket = aws_s3_bucket.my_bucket.bucket
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_kms_key" "my_key" {
+  description             = "KMS key for S3 encryption"
+  deletion_window_in_days = 7
+}
+
+
